03693bde144749929eaf8a8ace78442762d43f3f
Volodymyr Marchuk <wlodek.it@h5.digital>
2024-10-18 12:33:57 +0200

    added login/logout user adn update state

---
+++ src/components/Buttons/AddIconButton.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import AddCircleIcon from '@mui/icons-material/AddCircle'
+import { theme } from 'src/theme'
+import { IconButton } from '@mui/material'
+
+interface IAddIconButton {
+  onClick: () => void
+}
+
+export const AddIconButton = ({ onClick }: IAddIconButton) => (
+  <ButtonStyled onClick={onClick}>
+    <AddIconButtonStyled />
+  </ButtonStyled>
+)
+const ButtonStyled = styled(IconButton)`
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  padding: 0;
+`
+const AddIconButtonStyled = styled(AddCircleIcon)`
+  cursor: pointer;
+  fill: ${theme.accentColor};
+  width: 100%;
+  height: auto;
+  &:hover {
+    fill: green;
+  }
+`

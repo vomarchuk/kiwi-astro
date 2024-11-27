7c61cb7f44b3afcdea0b33731f2c606c3f1bd52f
Volodymyr Marchuk <wlodek.it@h5.digital>
2024-11-27 10:14:25 +0100

    finish main section

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -6,13 +6,6 @@ export const Main = () => {
     <main>
       <ServiceCarousel />
       <AboutSalon />
-      main section
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci neque
-        nemo nesciunt quas quaerat, rem tenetur repudiandae tempore accusamus
-        officia necessitatibus accusantium corporis minus. Deserunt quos
-        repellat sunt asperiores odit?
-      </p>
     </main>
   )
 }

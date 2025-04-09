5a8ecb62241196f08f5faa3bb2bd7d7b2b12c464
Volodymyr Marchuk <mr.volodymyr.marchuk@gmail.com>
2025-04-09 14:42:34 +0200

    change nav icon

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,78 +1,35 @@
-// src/components/Sidebar.jsx
-import React, { useState } from 'react'
-import styled from '@emotion/styled'
+import React, { useState } from "react";
+import styled from "@emotion/styled";
 import {
-  // Button,
-  // Container,
-  // Menu,
+  Menu,
   MenuItem,
-  AppBar,
   IconButton,
   Typography,
-  // Toolbar,
-  // Box,
+  Toolbar,
+  Box,
   Drawer,
-} from '@mui/material'
-import MenuIcon from '@mui/icons-material/Menu'
-import { LogoType } from '../LogoType/LogoType'
-// import { theme } from '../../theme'
-import { PAGES } from '../../constants/PAGES'
-import { Link } from '@tanstack/react-router'
-// import { ButtonReservation } from '../Buttons/ButtonReservation'
-// import { SocialList } from '../SocialList/SocialList'
-// import { SignInModal } from '../Modals/SignInModal'
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { LogoType } from "../LogoType/LogoType";
+import { PAGES } from "../../constants/PAGES";
+import { Link } from "@tanstack/react-router";
+import { SocialList } from "../SocialList/SocialList";
 const Sidebar = () => {
-  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null)
-  const [open, setOpen] = useState(false)
-  // const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) =>
-  //   setAnchorElNav(event.currentTarget)
-
-  // const handleOpenSignInModal = () => setOpen(true)
-  // const handleCloseSignInModal = () => setOpen(false)
-  const handleCloseNavMenu = () => setAnchorElNav(null)
-
-  const toggleDrawer = (newOpen: boolean) => () => {
-    setOpen(newOpen)
-  }
-
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+  const [open, setOpen] = useState(false);
+  const handleCloseNavMenu = () => setAnchorElNav(null);
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) =>
+    setAnchorElNav(event.currentTarget);
+  //
   return (
     <>
-      <IconButton
-        size="large"
-        aria-label="account of current user"
-        aria-controls="menu-appbar"
-        aria-haspopup="true"
-        onClick={toggleDrawer(true)}
-        color="inherit"
-        sx={{
-          width: '45px',
-          height: '45px',
-          // backgroundColor: 'white',
-          // position: 'absolute',
-          // right: 0,
-        }}
-      >
-        <MenuIcon sx={{ fill: 'white', width: '35px', height: '35px' }} />
-      </IconButton>
-
-      <Drawer anchor="right" open={open} onClose={toggleDrawer(false)}>
-        {/* // {DrawerList} */}
-        {PAGES.map((page) => (
-          <MenuItem key={page.id} onClick={handleCloseNavMenu}>
-            <LinkStyled to={`/services?id=${page.id}`}>
-              <Typography sx={{ textAlign: 'center' }}>{page.name}</Typography>
-            </LinkStyled>
-          </MenuItem>
-        ))}
-      </Drawer>
-      {/* <SignInModal open={open} handleClose={handleCloseSignInModal} /> */}
-      {/* <Toolbar disableGutters>
+      <Toolbar disableGutters>
         <Box
           sx={{
             flexGrow: 1,
-            display: 'flex',
-            alignItems: 'center',
-            padding: '5px 0px',
+            display: "flex",
+            alignItems: "center",
+            padding: "5px 0px",
           }}
         >
           <IconButton
@@ -83,8 +40,9 @@ const Sidebar = () => {
             onClick={handleOpenNavMenu}
             color="inherit"
             sx={{
-              width: '45px',
-              height: '45px',
+              width: "45px",
+              height: "45px",
+              color: "white",
             }}
           >
             <MenuIcon />
@@ -93,13 +51,13 @@ const Sidebar = () => {
             id="menu-appbar"
             anchorEl={anchorElNav}
             anchorOrigin={{
-              vertical: 'bottom',
-              horizontal: 'left',
+              vertical: "bottom",
+              horizontal: "left",
             }}
             keepMounted
             transformOrigin={{
-              vertical: 'top',
-              horizontal: 'left',
+              vertical: "top",
+              horizontal: "left",
             }}
             open={Boolean(anchorElNav)}
             onClose={handleCloseNavMenu}
@@ -107,92 +65,21 @@ const Sidebar = () => {
             {PAGES.map((page) => (
               <MenuItem key={page.id} onClick={handleCloseNavMenu}>
                 <LinkStyled to={`/services?id=${page.id}`}>
-                  <Typography sx={{ textAlign: 'center' }}>
+                  <Typography sx={{ textAlign: "center" }}>
                     {page.name}
                   </Typography>
                 </LinkStyled>
               </MenuItem>
             ))}
           </Menu>
-          <LogoTypeS width={120} onClick={handleOpenSignInModal} />
         </Box>
-        <SocialList />
-      </Toolbar> */}
+      </Toolbar>
     </>
-  )
-}
+  );
+};
 
-export default Sidebar
-
-const LogoTypeS = styled(LogoType)`
-  position: absolute;
-  left: 50%;
-  transform: translateX(-50%);
-`
-
+export default Sidebar;
 const LinkStyled = styled(Link)`
   text-decoration: none;
   color: black;
-`
-const LinkMobileStyled = styled(Link)`
-  text-decoration: none;
-`
-
-// import * as React from 'react'
-// import Box from '@mui/material/Box'
-// import Drawer from '@mui/material/Drawer'
-// import Button from '@mui/material/Button'
-// import List from '@mui/material/List'
-// import Divider from '@mui/material/Divider'
-// import ListItem from '@mui/material/ListItem'
-// import ListItemButton from '@mui/material/ListItemButton'
-// import ListItemIcon from '@mui/material/ListItemIcon'
-// import ListItemText from '@mui/material/ListItemText'
-// import InboxIcon from '@mui/icons-material/MoveToInbox'
-// import MailIcon from '@mui/icons-material/Mail'
-
-// export default function TemporaryDrawer() {
-//   const [open, setOpen] = React.useState(false)
-
-//   const toggleDrawer = (newOpen: boolean) => () => {
-//     setOpen(newOpen)
-//   }
-
-//   const DrawerList = (
-//     <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
-//       <List>
-//         {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-//           <ListItem key={text} disablePadding>
-//             <ListItemButton>
-//               <ListItemIcon>
-//                 {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-//               </ListItemIcon>
-//               <ListItemText primary={text} />
-//             </ListItemButton>
-//           </ListItem>
-//         ))}
-//       </List>
-//       <Divider />
-//       <List>
-//         {['All mail', 'Trash', 'Spam'].map((text, index) => (
-//           <ListItem key={text} disablePadding>
-//             <ListItemButton>
-//               <ListItemIcon>
-//                 {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-//               </ListItemIcon>
-//               <ListItemText primary={text} />
-//             </ListItemButton>
-//           </ListItem>
-//         ))}
-//       </List>
-//     </Box>
-//   )
-//   return (
-//     <div>
-//       <Button onClick={toggleDrawer(true)}>Open drawer</Button>
-//       <Drawer open={open} onClose={toggleDrawer(false)}>
-//         {DrawerList}
-//       </Drawer>
-//     </div>
-//   )
-// }
+`;
